5dcf203e2ae862ded4ba374865904c20908de874
TangiLC <tangi@le-cadre.net>
2023-05-03 11:17:05 +0200

    corrections

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -5,14 +5,36 @@
 import { screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import { ROUTES_PATH} from "../constants/routes.js";
+import router from "../app/Router.js";
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
+    test("Then submiting form creates new entry", () => {
       const html = NewBillUI()
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.NewBill)
       document.body.innerHTML = html
-      //to-do write assertion
+      const handleSubmit1 =jest.fn()
+      
+
+      handleSubmit1.handleSubmit()
+    expect(handleSubmit1()).toBeCalled()
+    })
+
+    test("Then selecting a .jpg files adds file", () => {
+      const html = NewBillUI()
+      screen.innerHTML = html
+      const handleChangeFile1 =jest.fn()
+
+
+      handleChangeFile1.handleChangeFile()
+    expect(handleChangeFile1()).toBeCalled()
+
     })
   })
 })
